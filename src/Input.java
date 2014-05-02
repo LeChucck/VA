aaf98bf64e44000a94e279714bd8695e10d625cc
vholmer <viktorholmer@gmail.com>
2014-05-02 14:59:47 +0200

    lel

--- src/Input.java
+++ src/Input.java
@@ -2,4 +2,12 @@
  * Created by vholmer on 02/05/14.
  */
 public class Input {
+
+    public Input() {
+        checkKeyPresses();
+    }
+
+    private void checkKeyPresses() {
+
+    }
 }
--- src/Renderer.java
+++ src/Renderer.java
@@ -10,6 +10,8 @@ public class Renderer {
     public void start() {
         try {
             Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+            Display.setInitialBackground(30f, 40f, 255f);
+            Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
             System.exit(0);
@@ -18,10 +20,13 @@ public class Renderer {
         while(!Display.isCloseRequested()) {
             Display.update();
         }
+
+        Display.destroy();
     }
 
     public static void main(String[] args) {
         Renderer renderer = new Renderer();
+        Input playerInput = new Input();
         renderer.start();
     }
 }
